4ff376cf157e80c0f25857f30f39784e280d5176
Heaple <93637073+Heaple@users.noreply.github.com>
2022-07-29 23:19:21 +0900

    [Gold V] Title: ABCDE, Time: 1168 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [[] for i in range(n)]
+visited = [False] * n
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+
+def dfs(a, n):
+    if n == 4:
+        print(1)
+        exit()
+    for i in arr[a]:
+        if not visited[i]:
+            visited[i] = True
+            dfs(i, n + 1)
+            visited[i] = False
+
+for i in range(n):
+    visited[i] = True
+    dfs(i, 0)
+    visited[i] = False
+
+print(0)
