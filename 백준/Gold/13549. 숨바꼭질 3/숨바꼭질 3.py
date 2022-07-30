dd7ea4c3b2f6388be530a03bc1cf6e61d7fc94de
Heaple <93637073+Heaple@users.noreply.github.com>
2022-07-30 15:16:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 164 ms, Memory: 33668 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+N, K = map(int, input().split())
+v = [0 for _ in range(K+1)]
+q = deque()
+q.append((N, 0))
+v = [0]*100001
+v[N] = 1
+while q:
+    cur, x = q.popleft()
+    v[cur] = 1
+    if cur == K:
+        print(x)
+        break
+    if cur*2 < 100001 and not v[cur*2]:
+        q.append((cur*2, x))
+        v[cur*2] = 1
+    if 0 <= cur-1 and not v[cur-1]:
+        q.append((cur-1, x+1))
+        v[cur-1] = 1
+    if cur+1 < 100001 and not v[cur+1]:
+        q.append((cur+1, x+1))
+        v[cur+1] = 1
