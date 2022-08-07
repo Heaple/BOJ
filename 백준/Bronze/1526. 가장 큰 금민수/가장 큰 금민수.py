931729bb096c837b29b8e98987db90e7480309a9
Heaple <93637073+Heaple@users.noreply.github.com>
2022-08-07 11:46:34 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 280 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,11 +1,10 @@
 N = int(input())
-res = []
-for i in range(1, N+1):
+res = 0
+for i in range(N, 0, -1):
     a = str(i)
     for j in range(len(a)):
         if a[j] != '4' and a[j] != '7':
             break
     else:
-        res = i
-
-print(res)
+        print(i)
+        break
