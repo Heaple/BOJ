fb45b6f40fc9cc7e5955a060b98152336344d254
Heaple <93637073+Heaple@users.noreply.github.com>
2022-08-21 10:44:08 +0900

    [Silver IV] Title: 숫자 정사각형, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -5,13 +5,21 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 rectangle = [input() for _ in range(N)]
 res = 0
-for i in range(N):
-    for j in range(M):
-        for k in range(min(N, M), 0, -1):
+for k in range(min(N, M), 0, -1):
+    for i in range(N):
+        for j in range(M):
+            # print(k)
             if i+k < N and j+k < M and 0 <= i+k and 0 <= j+k:
                 if rectangle[i][j] == rectangle[i][j+k] == rectangle[i+k][j+k] == rectangle[i+k][j]:
-                    res = max(res, (k+1)**2)
-if res == 0:
-    print(1)
-else:
-    print(res)
+                    if 0 < (k+1)**2:
+                        print((k+1)**2)
+                        quit()
+                    # else:
+                    #     print(1)
+                    # res = max(res, )
+
+print(1)
+# if res == 0:
+#     print(1)
+# else:
+#     print(res)
