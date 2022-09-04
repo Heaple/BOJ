e6525f1ef25c47d7a729caa7fc32ff3abb938d7f
Heaple <93637073+Heaple@users.noreply.github.com>
2022-09-04 10:42:54 +0900

    [Silver V] Title: 단어 정렬, Time: 96 ms, Memory: 32884 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,14 +1,17 @@
 import sys
 
 input = sys.stdin.readline
+print = sys.stdout.write
 
 N = int(input())
 w = [input().rstrip() for _ in range(N)]
 
 w.sort()
 w.sort(key=lambda x: len(x))
-a = []
-for i in w:
-    if i not in a:
-        print(i)
-        a.append(i)
+#a = []
+#s = ''
+print(w[0]+'\n')
+# print()
+for i in range(1,len(w)):
+    if w[i] != w[i-1]:
+        print(w[i]+"\n")
